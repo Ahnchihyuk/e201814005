50531fc227183ead3be80f96845abecafcb4827f
Ahnchihyuk <clgurdl99@gmail.com>
2024-09-25 09:32:59 +0900

    fifth

--- src/e201814005/World.java
+++ src/e201814005/World.java
@@ -5,4 +5,8 @@ public class World {
     public static void run() {
         System.out.println("201814005");
     }
+
+    public static void run2() {
+        System.out.println("hello world");
+    }
 }
